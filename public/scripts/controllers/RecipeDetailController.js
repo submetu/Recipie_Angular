6ac7821e2edc460ca0149d148d0d18ccc38df1fc
M Subhan Naeem <sub.metu@gmail.com>
2016-11-06 23:18:56 +0200

    Added display of recipe information to be edited in selects and inputs

--- public/scripts/controllers/RecipeDetailController.js
+++ public/scripts/controllers/RecipeDetailController.js
@@ -1,10 +1,21 @@
 angular.module('app')
-.controller('RecipeDetailController',['recipeService','$location',function(recipeService,$location){
+.controller('RecipeDetailController',['recipeService','$location','dataService',function(recipeService,$location,dataService){
 	var self = this;
 
-	self.recipe = recipeService.recipe;
-	console.log(self.recipe)
+	self.recipe = recipeService.recipe; //THE RECIPE THAT WAS CLICKED TO BE EDITED
 	
+	// console.log(self.recipe.category)
+	self.selectRecipes = dataService.query(); //GET ALL THE RECIPES
+
+	self.defaultSelect = function(){
+		for(var recipe in self.selectRecipes){
+			if(self.recipe.category === recipe.category){
+				return recipe.category;
+			}
+		}
+	}
+
+
 	//WEHEN THE USER CLICKS THE CANCEL BUTTON TAKE THEM TO THE MAIN PAGE
 	self.cancelButton = function(){
 		$location.path('/');
